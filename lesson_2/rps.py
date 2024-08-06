cd14390f99d326423788ff6314d1eb5c44c81a3b
Ryan Do <rdo9313@gmail.com>
2024-08-06 01:40:01 -0400

    rpsls bonus features

--- lesson_2/rps.py
+++ lesson_2/rps.py
@@ -1,5 +1,7 @@
 import random
+import os
 
+WINNING_SCORE = 3
 VALID_SHORTENED_CHOICES = ['r', 'p', 's', 'l', 'sp']
 VALID_CHOICES = ['rock', 'paper', 'scissors', 'lizard', 'spock']
 WINNING_COMBOS = {
@@ -13,63 +15,115 @@ WINNING_COMBOS = {
 def prompt(message):
     print(f"==> {message}")
 
+def wait_for_input():
+    input("Press any key to continue:")
+    clear_screen()
+
+def clear_screen():
+    os.system("clear")
+
 def display_welcome_message():
-    prompt(f"""Welcome to RPSLS. 
+    clear_screen()
+    prompt(f"""Welcome to RPSLS.
 Go to https://www.wikihow.com/Play-Rock-Paper-Scissors-Lizard-Spock for rules.
-Press any key to continue:""")
+First to {WINNING_SCORE} wins will claim victory.""")
+
+def display_goodbye_message():
+    clear_screen()
+    prompt("Thanks for playing RPSLS!")
+
+def display_choices(player, computer):
+    clear_screen()
+    prompt(f"You chose {player}, computer chose {computer}.")
+
+def ask_play_again():
+    prompt("Do you want to play again? (y/n)")
+
+def display_winner(score):
+    if score["player"] == WINNING_SCORE:
+        prompt("Player won. Congratulations!")
+    else:
+        prompt("Computer won. Better luck next time!")
 
 def determine_winner(player, computer):
     if computer in WINNING_COMBOS[player]:
-        return player
-    elif player in WINNING_COMBOS[computer]:
-        return computer
-    else:
-        return "tie"
+        return "player"
+    if player in WINNING_COMBOS[computer]:
+        return "computer"
 
-def display_winner(player, computer):
-    if ((player == "rock" and computer == "scissors") or
-        (player == "paper" and computer == "rock") or
-        (player == "scissors" and computer == "paper")):
-        prompt("You win!")
-    elif ((player == "rock" and computer == "paper") or
-        (player == "paper" and computer == "scissors") or
-        (player == "scissors" and computer == "rock")):
+    return "tie"
+
+def display_result(winner, score):
+    if winner == "player":
+        prompt("Player wins!")
+    elif winner == "computer":
         prompt("Computer wins!")
     else:
         prompt("It's a tie!")
 
+    prompt(f"Player: {score["player"]} - Computer: {score["computer"]}")
+    wait_for_input()
+
 def retrieve_player_choice():
     choice = input().strip().lower()
 
-    while choice not in (VALID_CHOICES or VALID_SHORTENED_CHOICES):
+    while choice not in (VALID_CHOICES and VALID_SHORTENED_CHOICES):
         prompt("That's not a valid choice.")
         choice = input().strip().lower()
-    
+
+    if choice in VALID_SHORTENED_CHOICES:
+        choice = VALID_CHOICES[VALID_SHORTENED_CHOICES.index(choice)]
+
     return choice
 
 def retrieve_computer_choice():
     return random.choice(VALID_CHOICES)
 
+def retrieve_yes_or_no():
+    answer = input().strip().lower()
+
+    while not valid_yes_or_no(answer):
+        prompt("Please input a valid answer (y/n)")
+        answer = input().strip().lower()
+
+    clear_screen()
+    return (answer in ['y', 'yes'])
+
+def update_score(winner, score):
+    if winner == "player":
+        score["player"] += 1
+    elif winner == "computer":
+        score["computer"] += 1
+
+def valid_yes_or_no(answer):
+    return answer in ['y', 'yes', 'n', 'no']
+
 display_welcome_message()
+wait_for_input()
 
-while True:
-    prompt(f"Choose one: {', '.join(VALID_CHOICES)} ({', '.join(VALID_SHORTENED_CHOICES)})")
-    choice = retrieve_player_choice()
-
-    computer_choice = retrieve_computer_choice()
-
-    prompt(f"You chose {choice}, computer chose {computer_choice}")
-
-    display_winner(choice, computer_choice)
-
-    prompt("Do you want to play again? (y/n)")
-    answer = input().lower()
+def main():
     while True:
-        if answer.startswith('n') or answer.startswith('y'):
+        score = {"player": 0, "computer": 0}
+        while max(score.values()) < WINNING_SCORE:
+            prompt(f"Choose one: {', '.join(VALID_CHOICES)} \
+({', '.join(VALID_SHORTENED_CHOICES)})")
+
+            player_choice = retrieve_player_choice()
+            computer_choice = retrieve_computer_choice()
+
+            display_choices(player_choice, computer_choice)
+
+            winner = determine_winner(player_choice, computer_choice)
+            update_score(winner, score)
+            display_result(winner, score)
+
+        display_winner(score)
+        ask_play_again()
+        play_again = retrieve_yes_or_no()
+
+        if not play_again:
             break
 
-        prompt("Please enter 'y' or 'n'.")
-        answer = input().lower()
-    
-    if answer[0] == 'n':
-        break
+    display_goodbye_message()
+
+main()
